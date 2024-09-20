a19d8f6d87c0f6b676fd3a18133f7d7b442873f9
YaoyiW27 <rmwyyy@gmail.com>
2024-09-19 19:31:51 -0700

    added Input Name area to App.js

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, SafeAreaView } from 'react-native';
+import React, { useState } from 'react';
 import Header from './components/Header';
+import Input from './components/Input';
 
 export default function App() {
   const appName = 'Welcome'
+  const [name, setName] = useState('');
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
         <View style={styles.topView}>
           <Header name={appName}></Header>
+          <Input
+            label="Name"
+            value={name}
+            onChangeText={(text) => setName(text)}
+          />
         </View>
     </SafeAreaView>
   );
