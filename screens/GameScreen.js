9236ed819c9ed2065ee02f5950a400fd56622f15
YaoyiW27 <rmwyyy@gmail.com>
2024-09-19 23:36:03 -0700

    added start button and initial instruction for game

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,8 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
 export default function GameScreen({ userInfo, onRestart }) {
   const [targetNumber, setTargetNumber] = useState(null);
+  const [gameStarted, setGameStarted] = useState(false);
+
+  const startGame = () => {
+    setGameStarted(true);
+    setTimeLeft(60);
+    setAttemptsLeft(4);
+    startTimer();
+  };
 
   useEffect(() => {
     if (userInfo) {
@@ -20,7 +28,17 @@ export default function GameScreen({ userInfo, onRestart }) {
 
   return (
     <View style={styles.container}>
-      {/* later */}
+      {!gameStarted ? (
+        <>
+          <Text style={styles.text}>
+            Guess a number between 1 and 100 that is multiply of {userInfo.phone.slice(-1)} 
+          </Text>
+          <Button title="Start" onPress={startGame} />
+        </>
+      ) : (
+        // page after
+        <View>{/* Game components */}</View>
+      )}
     </View>
   );
 }
@@ -29,5 +47,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
+  },
+  text: {
+    fontSize: 20,
+    color: 'blue',
+    textAlign: 'center',
+    marginBottom: 20,
   },
 });
